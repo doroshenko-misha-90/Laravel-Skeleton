da096929e1b1ae9d39dd93df449fc0501d3901e0
Aios <aioslike@gmail.com>
2019-03-01 17:06:26 +0200

    Init Skeleton

---
+++ modules/Users/Entities/Permission.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Users\Entities;
+
+use Laratrust\LaratrustPermission;
+
+class Permission extends LaratrustPermission
+{
+    //
+}
---
+++ modules/Users/Entities/Role.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Users\Entities;
+
+use Laratrust\LaratrustRole;
+
+class Role extends LaratrustRole
+{
+    //
+}
